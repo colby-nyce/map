eb3558836d834df555339beac74915c5891d368c
colby-nyce <colby-nyce@users.noreply.github.com>
2025-09-30 20:05:21 +0000

    [ci skip] Database-backed checkpointer (#605)
    * Refactor Sparta checkpointers
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Last set of changes before redesign
    
    * Database-backed checkpointer
    
    * Database-backed checkpointer
    
    * Update all doxygen and add more testing
    
    * Fix clang build/regress failures
    
    * Fix clang build/regress failures
    
    * Attempt to fix -LE issue with ctest (macos)
    
    * Align the apple/valgrind/ctest code in the test dir and example dir
    
    * Redirect std::cerr to a logfile for the Buffer_test
    
    * Address PR feedback
    
    * Address PR feedback
    
    * Remove old debug code
    
    * Add DatabaseCheckpointer::findLatestCheckpointAtOrBefore()
    
    * Remove O(n) code from ensureWindowLoaded_
    
    * Re-enable Buffer_test
    
    * Move DatabaseCheckpoint test dir
    
    * Change variable name in unit test
    
    * Bump SimDB
    
    * Bump SimDB
    
    * PR feedback
    
    * Bump SimDB 460ecced9a1f764bbadbe5cde7a2a26d530597f7

---
+++ navtreedata.js
@@ -0,0 +1,261 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "The Sparta Modeling Framework", "index.html", [
+    [ "Sparta", "index.html", null ],
+    [ "Best Practices, Suggestions on using Sparta", "best_practices.html", [
+      [ "Using Sparta Asserts", "best_practices.html#sparta_asserts", null ],
+      [ "Using Sparta compile-time hints", "best_practices.html#sparta_expect", null ],
+      [ "Take Advantage of C++11/17!", "best_practices.html#cpp_eleven", null ],
+      [ "How to Use the Port/Event Mechansim Effectively", "best_practices.html#discrete_event", [
+        [ "Phases", "best_practices.html#phases", null ],
+        [ "Using The Phases", "best_practices.html#using_phase", null ]
+      ] ]
+    ] ],
+    [ "Building and Integrating the Framework", "building.html", null ],
+    [ "Checkpointing", "checkpoint_format.html", null ],
+    [ "Command Command Line Interface", "common_cmdline.html", [
+      [ "CommandLineSimulator", "common_cmdline.html#cmdline_sim", [
+        [ "Logging Configuration", "common_cmdline.html#cmdline_log_cfg", null ],
+        [ "Command Line Simulation Configuration", "common_cmdline.html#cmdline_sim_cfg", null ]
+      ] ],
+      [ "Example Output", "common_cmdline.html#example_cmdline_help_out", null ]
+    ] ],
+    [ "Communication, Events, and Scheduling", "communication.html", [
+      [ "Ports", "communication.html#comm_ports", [
+        [ "sparta::SyncPorts", "communication.html#sync_ports", null ]
+      ] ],
+      [ "Events", "communication.html#comm_events", null ],
+      [ "Clocks", "communication.html#comm_clocks", null ],
+      [ "Scheduling", "communication.html#comm_schedulers", [
+        [ "sparta::Scheduler", "communication.html#sparta_Scheduler", null ],
+        [ "sparta::SysCSpartaSchedulerAdapter", "communication.html#sysc_scheduler", null ]
+      ] ]
+    ] ],
+    [ "Core Example Using Sparta", "core_example.html", [
+      [ "Example Core Layout", "core_example.html#example_core", null ],
+      [ "Building", "core_example.html#Building", null ],
+      [ "Invocations", "core_example.html#Invocations", [
+        [ "Running/Configuring", "core_example.html#run_config_core_example", null ],
+        [ "Debugging/Logging", "core_example.html#debugging_logging_core_example", null ],
+        [ "Generating Reports", "core_example.html#reporting_core_example", null ],
+        [ "Generating Pipeouts", "core_example.html#pipeouts_core_example", null ]
+      ] ]
+    ] ],
+    [ "Sparta Command Line Interface End-User Guide", "end_user.html", [
+      [ "1 Simulator Invocation", "end_user.html#invocation", [
+        [ "1.1 I/O Policies", "end_user.html#io_policies", null ],
+        [ "1.2 Sparta Basic Command-Line Options", "end_user.html#sparta_cmds", null ],
+        [ "1.3 Application-Specific Commands", "end_user.html#app_cmds", null ],
+        [ "1.4 Sparta Advanced commands", "end_user.html#sparta_advanced_cmds", null ],
+        [ "1.5 Sparta Simulation Debug commands", "end_user.html#sim_dbg_cmds", null ]
+      ] ],
+      [ "2 Control and Configuration", "end_user.html#ctrl_cfg", [
+        [ "2.1 Parameters", "end_user.html#ctrl_cfg_parameters", null ],
+        [ "2.2 Selecting Architectures", "end_user.html#ctrl_cfg_architecture", null ],
+        [ "2.3 Numeric Constants", "end_user.html#numeric_constants", null ]
+      ] ],
+      [ "3 Simulator Output", "end_user.html#output", [
+        [ "3.1 Automatic Summary", "end_user.html#auto_summary", null ],
+        [ "3.2 Report Generation", "end_user.html#report_gen", null ],
+        [ "3.3 Message Logging", "end_user.html#msg_logging", null ],
+        [ "3.4 Notification Logging", "end_user.html#notification_logging", null ],
+        [ "3.5 Performance Events", "end_user.html#perf_events", null ],
+        [ "3.6 Pipeline Collection", "end_user.html#pipeline_collection", null ],
+        [ "3.7 Post-Run Debug dumps", "end_user.html#debug_dump", null ],
+        [ "3.4 Backtraces", "end_user.html#backtraces", null ],
+        [ "3.8 Device Tree Inspection", "end_user.html#tree_inspection", null ]
+      ] ],
+      [ "4 Running with a debugger", "end_user.html#run_with_debugger", [
+        [ "GDB", "end_user.html#run_with_dbg_gdb", null ],
+        [ "Other Debuggers", "end_user.html#run_with_dbg_other", null ]
+      ] ],
+      [ "4 Post-processing and Visualization", "end_user.html#data_proc_vis", [
+        [ "4.1 Pipeline viewer (Argos)", "end_user.html#argos", null ]
+      ] ]
+    ] ],
+    [ "Sparta FAQ", "sparta_faq.html", [
+      [ "Modeling Determinism", "sparta_faq.html#modeling_determinism", null ],
+      [ "DES Debugging Techniques", "sparta_faq.html#des_debugging", null ]
+    ] ],
+    [ "Sparta File Formats", "formats.html", null ],
+    [ "Parameter/Configuration Format (.cfg,.yaml)", "param_format.html", [
+      [ "Nesting", "param_format.html#param_format_nesting", null ],
+      [ "Parameter Assignment Attributes and Optional Parameters", "param_format.html#param_format_attributes", null ],
+      [ "Examples", "param_format.html#param_format_examples", null ]
+    ] ],
+    [ "Report Definition Format (.rrep,.yaml)", "report_def_format.html", [
+      [ "Overview", "report_def_format.html#report_def_overview", null ],
+      [ "Structure", "report_def_format.html#report_def_structure", [
+        [ "Report Fields", "report_def_format.html#report_def_field", null ],
+        [ "Example Report Definition", "report_def_format.html#report_def_example", null ],
+        [ "Field Declarations", "report_def_format.html#report_def_field_declarations", null ],
+        [ "Field Name Variables", "report_def_format.html#report_def_field_name_variables", null ],
+        [ "Subreports", "report_def_format.html#report_def_subreports", null ],
+        [ "Scope Wildcards", "report_def_format.html#report_def_scope_wildcards", null ],
+        [ "Statistical Expressions", "report_def_format.html#report_def_expressions", null ],
+        [ "Include Directives", "report_def_format.html#report_def_includes", null ],
+        [ "Style section", "report_def_format.html#report_def_style", null ],
+        [ "Autopopulation Blocks", "report_def_format.html#report_def_autopop", null ]
+      ] ],
+      [ "Field Declaration Path/Expression Ambiguity", "report_def_format.html#report_def_var_name_ambiguity", null ],
+      [ "Report Definition Directive and Option Reference", "report_def_format.html#report_def_directive_ref", null ],
+      [ "Limitations of Report Definitions", "report_def_format.html#report_def_lims", null ]
+    ] ],
+    [ "Report Output Formats", "report_out_format.html", [
+      [ "Plaintext (.txt, .text)", "report_out_format.html#report_out_format_plaintext", null ],
+      [ "CSV (.csv)", "report_out_format.html#report_out_format_csv", null ],
+      [ "BasicHTML (.html, .htm)", "report_out_format.html#report_out_format_basichtml", null ],
+      [ "Gnuplot (.gnuplot, .gplt)", "report_out_format.html#report_out_format_gnuplot", null ],
+      [ "PythonDict (.python, .python)", "report_out_format.html#report_out_format_pythondict", null ],
+      [ "JavascriptObject (.json)", "report_out_format.html#report_out_format_json", null ]
+    ] ],
+    [ "Logging Output formats (.log, .log.raw, .log.basic, log.verbose)", "log_out_format.html", null ],
+    [ "Framework Development", "framework_dev.html", [
+      [ "Coding Style", "framework_dev.html#coding_style", null ],
+      [ "Regression Testing", "framework_dev.html#regression", null ],
+      [ "Doxygen Convention", "framework_dev.html#doxygen_convention", [
+        [ "Code Documentation Convention", "framework_dev.html#code_conv", null ],
+        [ "Textual Documentation Convention", "framework_dev.html#text_conv", null ]
+      ] ],
+      [ "Meta Documentation", "framework_dev.html#meta_doc", null ]
+    ] ],
+    [ "Textual Message Logging", "logging.html", [
+      [ "Logging System Goals", "logging.html#logging_goals", null ],
+      [ "Logging System Design Requirements", "logging.html#log_des_reqs", null ],
+      [ "Conceptual Usage", "logging.html#logg_concept_usage", [
+        [ "Scoped Logging", "logging.html#scoped_log", null ],
+        [ "Global Logging", "logging.html#global_log", null ]
+      ] ],
+      [ "Capturing Log Messages", "logging.html#log_capturing", null ],
+      [ "Usage Notes", "logging.html#log_usage_notes", null ],
+      [ "Implementation Notes", "logging.html#log_impl_notes", null ]
+    ] ],
+    [ "Sparta API", "modeling.html", null ],
+    [ "Simulator Configuration", "config.html", [
+      [ "Configuration Goals", "config.html#config_goals", null ],
+      [ "Simulator Subclass Configuration", "config.html#config_usage", null ],
+      [ "Configuration System Design Requirements", "config.html#config_des_req", null ]
+    ] ],
+    [ "Skeleton Pipeline Using Sparta", "skeleton_example.html", [
+      [ "Skeleton Pipeline Layout", "skeleton_example.html#skeleton_layout", [
+        [ "Getting Help and Simulation Layout", "skeleton_example.html#autotoc_md25", null ],
+        [ "Running the simulator", "skeleton_example.html#autotoc_md26", null ],
+        [ "Running the simulator and generating reports", "skeleton_example.html#autotoc_md27", null ],
+        [ "Running the simulator and generating logs", "skeleton_example.html#autotoc_md28", null ],
+        [ "Running the simulator and configuring it", "skeleton_example.html#autotoc_md29", null ],
+        [ "Looking at the Code", "skeleton_example.html#skeleton_looking", null ]
+      ] ]
+    ] ],
+    [ "SystemC Models", "systemc.html", [
+      [ "Building", "systemc.html#building_sysc", null ],
+      [ "SystemC/Sparta Adapter", "systemc.html#sysc_adapter", null ]
+    ] ],
+    [ "Precedence operators for EventNode/Scheduleables", "precedence_rules.html", null ],
+    [ "RegisterProxy Usage", "RegisterProxy_Usage.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AddressTypes_8hpp_source.html",
+"MessageInfo_8hpp_source.html",
+"SpartaHandler_8hpp_source.html",
+"classcore__example_1_1Execute_1_1ExecuteParameterSet.html",
+"classsparta_1_1Array.html#a469f43e31ed089cede106dd07254ffa9",
+"classsparta_1_1CircularBuffer.html#a19bef47b1788058cd0d2cbe51f2003b4",
+"classsparta_1_1CycleHistogramBase.html#a00b2b8ce1da7a36b43b9b5af4b6d5e2e",
+"classsparta_1_1DataView.html#ad13edf5ba9fc3d2b02046bfb4b43a211",
+"classsparta_1_1HistogramBase.html#a38fa6be9773dcbcd2b78f427aed0346a",
+"classsparta_1_1NotificationSourceBase.html#a476d4d8ae1133d22cbfee796ff0a2c76",
+"classsparta_1_1ParameterBase.html#a13a637320d9e37896abc10576d4a9219",
+"classsparta_1_1ParameterTree.html#a481b54e97eb57da8c5615f0581f8e360",
+"classsparta_1_1PhasedUniqueEvent.html#a973b084a7f33fdabcc2fb8a09148fc71",
+"classsparta_1_1Port.html#a170bc56dbbcf775943fad9c1a80273dd",
+"classsparta_1_1RegisterBankTable.html#acd0ae32acc56188c015ee45d92cb414f",
+"classsparta_1_1RegisterSet.html#a430e11c120e6ff27ba380344f29bbffb",
+"classsparta_1_1RootTreeNode.html#a4b9d3d82653ad11e9c2942efd1fd1804",
+"classsparta_1_1Scheduler.html#afe856f86b15acaa3479dda80ce338d3b",
+"classsparta_1_1SpartaSharedPointerAllocator.html#a85e059a11a23bcd39d29b27eb369a260",
+"classsparta_1_1StatisticInstance.html#a547f8af2c653d30f13d42fd62611bf73",
+"classsparta_1_1TieredMap.html#a4675bde6335ae7078a8ababeeede54a3",
+"classsparta_1_1TreeNode.html#a8e0703ca8742ef2de67a0d876ccb0c6e",
+"classsparta_1_1Unit.html#adc39dc12b225483f222fd6627c623f94",
+"classsparta_1_1app_1_1DefaultValues.html#a8e1d3ea08a41f910247941fca67ae251a2e979835dd62324f5bfe217449ba4974",
+"classsparta_1_1app_1_1Simulation.html#a200bdd31a21e24a242770fcb8e7fff5f",
+"classsparta_1_1app_1_1SimulationConfiguration.html#a50e61a9b28c921af9e74e3b53ebc5f2c",
+"classsparta_1_1collection_1_1PipelineCollector.html#a11b747e5d5792c6c64524bae1e389c96",
+"classsparta_1_1memory_1_1BlockingMemoryIFNode.html#ac8938cdf10f2f039336d8a7adb1175eb",
+"classsparta_1_1memory_1_1TranslationIFNode.html#a48d98b21f04c131bbf588249e6798179",
+"classsparta_1_1statistics_1_1ArchiveDataSeries.html#a675be03d5ab0622c4e188e36684e449a",
+"classsparta_1_1statistics_1_1expression_1_1grammar_1_1ExpressionGrammar.html",
+"classsparta_1_1utils_1_1ValidValue.html#a322b7620a53faa1e99092a8184add950",
+"namespacesparta.html#a2c04c9473be440ddcb25c2294fe90ec3",
+"structMetaStruct_1_1remove__any__pointer_3_01std_1_1shared__ptr_3_01T_01_4_01_4.html",
+"structsparta_1_1log_1_1MessageInfo.html#ad0b6578655aa109d2255bb352a9094e6",
+"structsparta_1_1statistics_1_1expression_1_1UnaryFunction.html#a7f0b89e0c1a9c68358c52a7ac6a9a9ac"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
