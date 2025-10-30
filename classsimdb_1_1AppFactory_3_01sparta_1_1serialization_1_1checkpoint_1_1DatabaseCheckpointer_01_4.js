244c9c8d3712bff80fac3878477d576b85a2a41a
colby-nyce <colby-nyce@users.noreply.github.com>
2025-10-30 14:39:43 +0000

    [ci skip] Fix bug where ignored (soft-deleted) windows trigger the no-dup-windows assertion (#618)
    As windows are processed, we only allow them to exist in exactly one
    spot: the cache, the pipeline, or the database. There are asserts in the
    code that ensure no duplicate windows are found, which is incorrectly
    firing in some use cases. Windows that are "deleted" in the pipeline
    (e.g. deleting future windows during `loadCheckpoint`) just get a flag
    "ignore=true" to save time. Finding "dups" in the pipeline, where e.g. 1
    is valid and 3 are ignored, should not count as 4 dups. 37da2df2e97cf91deb4e525bf88646d8a05b6927

---
+++ classsimdb_1_1AppFactory_3_01sparta_1_1serialization_1_1checkpoint_1_1DatabaseCheckpointer_01_4.js
@@ -0,0 +1,5 @@
+var classsimdb_1_1AppFactory_3_01sparta_1_1serialization_1_1checkpoint_1_1DatabaseCheckpointer_01_4 =
+[
+    [ "setArchDataRoot", "classsimdb_1_1AppFactory_3_01sparta_1_1serialization_1_1checkpoint_1_1DatabaseCheckpointer_01_4.html#affe9b7f99ca84e6b1c9aa306252a0e99", null ],
+    [ "setScheduler", "classsimdb_1_1AppFactory_3_01sparta_1_1serialization_1_1checkpoint_1_1DatabaseCheckpointer_01_4.html#ae53a7878e3340ce291780e0babe5e00d", null ]
+];
