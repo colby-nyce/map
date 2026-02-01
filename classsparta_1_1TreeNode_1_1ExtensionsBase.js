7efda8696deae23ec6f11f67d3675330b6da1279
colby-nyce <colby-nyce@users.noreply.github.com>
2026-02-01 18:57:09 +0000

    [ci skip] TreeNode extensions bug fixes (#642)
    This follow-up PR fixes the backwards compatibility issue discussed
    here:
    
    https://github.com/sparcians/map/issues/621
    
    The source of the bug boils down to this:
    - specify extensions in extension/arch/config file
    - run the simulation with `--write-final-config`
    - at runtime, register an extension factory
    - then create the extension with `getExtension()` (implicit) or
    `createExtension()` (explicit)
    - extension subclass `postCreate()` method is called
    - in `postCreate()`, add some more parameters with default values
    
    The bug as reported is that the final config YAML file only has the
    extension parameters that were given in the input YAML files. The
    default values added in `postCreate()` do not appear in the final config
    YAML file. This is different than the first extensions implementation
    where all parameters are written to the final config regardless.
    
    These changes have been tested against Olympia and all regressions pass
    with no changes required to the Olympia source code. I also manually
    verified that all parameters show up the same way as before in the final
    config using the ticket's repro command:
    
    `./olympia --write-final-config baseline.yaml --no-run`
    
    This was also qualified against internal simulators with the only source
    changes outlined in an internal ticket.
    
    I'll assign the ticket back to Zen to qualify/close it when this PR is
    merged. b78c3eddf1ab404b2158ccb95a12a0a03becd842

---
+++ classsparta_1_1TreeNode_1_1ExtensionsBase.js
@@ -0,0 +1,5 @@
+var classsparta_1_1TreeNode_1_1ExtensionsBase =
+[
+    [ "getParameterValueAs", "classsparta_1_1TreeNode_1_1ExtensionsBase.html#a51a75d7e3e470c547a08537d709def80", null ],
+    [ "getUUID", "classsparta_1_1TreeNode_1_1ExtensionsBase.html#a46bb6b8b4d355f4fe19bd6e20da6b14e", null ]
+];
